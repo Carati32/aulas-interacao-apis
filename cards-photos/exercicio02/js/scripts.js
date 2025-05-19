f5a1e1b82d45e3880f1e00a2aa405f553ca05bcb
Carati32 <brunosilva33357@gmail.com>
2025-05-19 19:24:04 -0300

    Remoção de linha inútil

--- cards-photos/exercicio02/js/scripts.js
+++ cards-photos/exercicio02/js/scripts.js
@@ -1,6 +1,5 @@
 fetch("https://dog.ceo/api/breeds/image/random")
 .then(response => response.json())
 .then(dogs => {
-    const container = document.getElementById('container');
-      container.innerHTML = `<img src=${dogs.message}>`
+    container.innerHTML = `<img src=${dogs.message}>`
 })
